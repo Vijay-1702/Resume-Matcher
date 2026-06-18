185f468041de5b5f7bc1216b7c45b69464d36373
Vijay-1702 <vijay2005.pogula@gmail.com>
2026-06-18 23:20:43 +0530

    Implement resume upload API

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi import UploadFile, File
+import os
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.get("/")
+def home():
+    return {"message": "Resume Matcher API"}
+
+
+@app.post("/upload/resume")
+async def upload_resume(
+    file: UploadFile = File(...)
+):
+    file_path = os.path.join(
+        UPLOAD_DIR,
+        file.filename
+    )
+
+    with open(file_path, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    return {
+        "message": "Resume uploaded",
+        "filename": file.filename
+    }
